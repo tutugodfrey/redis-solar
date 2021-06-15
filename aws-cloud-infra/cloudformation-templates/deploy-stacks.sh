840b570f45c1df9c05d3d8b7d20f63ddda7d215c
tutugodfrey <tutu_godfrey@yahoo.com>
2021-06-15 13:48:32 +0100

    Update App Configuration
    - Use remove the WebApp AutoScaling
    - Update Loadbalancer to direct traffic to Servers in App Autoscaling group
    - Made some changes to the stack-helper script, Security-group stack

--- aws-cloud-infra/cloudformation-templates/deploy-stacks.sh
+++ aws-cloud-infra/cloudformation-templates/deploy-stacks.sh
@@ -5,7 +5,7 @@
 create_stack redis-solar-vpc-stack redis-solar-vpc-stack.yml redis-solar-vpc-parameters.json;
 sleep 60;
 create_stack security-groups-stack security-groups-stack.yml security-groups-parameters.json;
-
+deploy_stack --stack-name redis-solar-instance-profile --template-file instance-profile.yml --capabilities CAPABILITY_NAMED_IAM
 sleep 60;
 create_stack loadbalancer-stack loadbalancer-stack.yml loadbalancer-parameters.json;
 create_stack jump-stack jump-stack.yml jump-parameters.json;
